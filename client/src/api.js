227c05d5651f4d859adfb784774a86b66804483a
Casey Wittrock <caseywittrockmn@gmail.com>
2023-11-21 15:27:31 -0600

    fix env with vite

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.REACT_APP_BASE_URL || "https://story-gen-api.onrender.com";
+  import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:3005";
 
 /** API Class.
  *
