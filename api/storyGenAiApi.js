54eb05dc7835f8999e915a07f3a4484f4c8241cf
Casey Wittrock <caseywittrockmn@gmail.com>
2024-07-29 14:30:24 -0500

    fix elevenlabs requests

--- api/storyGenAiApi.js
+++ api/storyGenAiApi.js
@@ -116,7 +116,7 @@ OpenAI.prototype.generateAudio = async (text) => {
     url: `https://api.elevenlabs.io/v1/text-to-speech/${ELEVENLABS_VOICE_ID}`,
     headers: {
       accept: "audio/mpeg",
-      "content-Type": "application/json",
+      "Content-Type": "application/json",
       "xi-api-key": ELEVENLABS_API_KEY,
     },
     data: {
